7867abd07bfbad3c7917281458cf354998d0044a
Duke Nukem <garrettduke87@gmail.com>
2020-08-23 16:53:01 -0600

    added more functions

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const startButton = document.getElementById("start-btn");
+const nextButton = document.getElementById("next-btn");
+
 const questionContainerElement = document.getElementById("question-container");
 const questionElement = document.getElementById("question");
 const answerButtonsElement = document.getElementById("answer-buttons");
@@ -8,7 +10,6 @@ let shuffledQuestions, currentQuestionIndex;
 startButton.addEventListener("click", startGame);
 
 function startGame() {
-    console.log("Started");
     startButton.classList.add("hide");
     // shuffles the questions with the Math.random subtracting .5 from the number to give us a number greater than .5 or less than .5 each time
     shuffledQuestions = questions.sort(() => Math.random() - .5);
@@ -19,26 +20,53 @@ function startGame() {
 
 
 function setNextQuestion() {
+    resetState()
     showQuestion(shuffledQuestions[currentQuestionIndex]);
 }
 
 function showQuestion(question) {
-    questionElement.innerText = question.question
+    questionElement.innerText = question.question;
     question.answers.forEach(answer => {
-        const button = document.createElement("button")
-        button.innerText = answer.text
-        button.classList.add("btn")
+        const button = document.createElement("button");
+        button.innerText = answer.text;
+        button.classList.add("btn");
         if (answer.correct) {
             button.dataset.correct = answer.correct
-        }
-        button.addEventListener("click", selectAnswer)
-        answerButtonsElement.appendChild(button)
+        };
+        button.addEventListener("click", selectAnswer);
+        answerButtonsElement.appendChild(button);
     })
 };
 
+function resetState () {
+    nextButton.classList.add("hide")
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild;
+        (answerButtonsElement.firstChild);
+    }
+};
 
 function selectAnswer(e) {
+    const selectedButton = e.target;
+    const correct = selectedButton.dataset.correct;
+    setStatusClass(document.body, correct);
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct);
+    })
+};
 
+function setStatusClass(element, correct) {
+    clearStatusClass(element);
+    if (correct) {
+        element.classList.add("correct")
+    } else {
+        element.classList.add("incorrect");
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove("correct");
+    element.classList.remove("incorrect");
 }
 
 const questions = [
@@ -51,4 +79,4 @@ const questions = [
             { text: "Varment", correct: false }
         ]
     }
-]
+];
